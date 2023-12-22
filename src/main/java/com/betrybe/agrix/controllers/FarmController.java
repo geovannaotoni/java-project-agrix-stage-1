203eeafd914b63873ac8bfed5b9a9e194ab4d62a
Geovanna Otoni <geovannaotoni@gmail.com>
2023-12-22 10:24:12 -0300

    feat: create endpoint GET /farms

--- src/main/java/com/betrybe/agrix/controllers/FarmController.java
+++ src/main/java/com/betrybe/agrix/controllers/FarmController.java
@@ -3,9 +3,11 @@ package com.betrybe.agrix.controllers;
 import com.betrybe.agrix.controllers.dto.FarmDto;
 import com.betrybe.agrix.models.entities.Farm;
 import com.betrybe.agrix.services.FarmService;
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -28,5 +30,18 @@ public class FarmController {
   public ResponseEntity<FarmDto> createFarm(@RequestBody FarmDto farmDto) {
     Farm newFarm = farmService.createFarm(farmDto.toFarm());
     return ResponseEntity.status(HttpStatus.CREATED).body(FarmDto.toDto(newFarm));
+  }
+
+  /**
+   * Gets all farms.
+   *
+   * @return the all farms
+   */
+  @GetMapping
+  public ResponseEntity<List<FarmDto>> getAllFarms() {
+    List<Farm> allFarms = farmService.getAllFarms();
+    List<FarmDto> allFarmsDto = allFarms.stream()
+        .map(farm -> FarmDto.toDto(farm)).toList();
+    return ResponseEntity.ok(allFarmsDto);
   }
 }
